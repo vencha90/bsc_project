627c22fb10e73863c564e3606c8eb7b7a4c834c1
Karlis Venters <kaarlis.venters@gmail.com>
2014-05-08 18:36:46 +0100

    Analysis uses correct types

--- Data/analysis.R
+++ Data/analysis.R
@@ -7,6 +7,8 @@ benchmark_file <- "benchmark.log"
 
 names <- setNames(1:8, c("time",    "action",    "reward",  "busy_for", "location",  "destination", "passenger", "fare"))
 column_types <-        c('numeric', 'character', 'numeric', 'numeric',  'character', 'character',   'character', 'numeric')
+numeric_names <- c('time', 'reward', 'busy_for', 'fare')
+factor_names <- c('action', 'location', 'destination', 'passenger')
 
 format_data <- function(file) {
   unformatted_data <- c(readLines(file))
@@ -19,7 +21,7 @@ format_data <- function(file) {
 
 assign <- function(data, names){
   pairs <- sapply(data, function(i) i)
-  result <- rep(NA, length(names))
+  result <- rep(c(NA), length(names))
   keys <- names[pairs[1, ]]
   values <- pairs[2, ]
   result[keys] <- values
@@ -30,10 +32,20 @@ data_table <- function(data) {
   sx <- lapply(strsplit(data, ";"), strsplit, "=")
   ret <- t(sapply(sx, assign, names))
   colnames(ret) <- names(names)
-  res <- as.data.frame(ret)
+  res <- as.data.frame(ret, na.strings=NA, stringsAsFactors=FALSE)
   res
 }
 
+fix_types <- function(data) {
+  for(name in numeric_names) {
+    data[, name] <- sapply(data[, name], as.numeric)
+  }
+  for(name in factor_names) {
+    data[, name] <- sapply(data[, name], as.factor)
+  }
+  data
+}
 
-normal_data <- data_table(format_data(normal_file))
-benchmark_data <- data_table(format_data(benchmark_file))
+normal_data <- fix_types(data_table(format_data(normal_file)))
+summary(normal_data)
+# benchmark_data <- fix_types(data_table(format_data(benchmark_file)))
