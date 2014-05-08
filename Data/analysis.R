c6fcb2b6a5c0f1511d48fca84e787cfa171f99e5
Karlis Venters <kaarlis.venters@gmail.com>
2014-05-08 14:52:33 +0100

    DRY code for analysis

--- Data/analysis.R
+++ Data/analysis.R
@@ -1,17 +1,21 @@
 # Adapted from
 # http://stackoverflow.com/questions/12913446/efficiently-create-dataframe-from-strings-containing-key-value-pairs
 
-setwd("c:\\Users/user/Dropbox/Documents/ABDN/Y4/PROJECT/Data/")
-file <- "standard/test.log"
+setwd("c:\\Users/user/Dropbox/Documents/ABDN/Y4/PROJECT/Data/standard/")
+normal_file <- "normal.log"
+benchmark_file <- "benchmark.log"
 
 names <- setNames(1:8, c("time",    "action",    "reward",  "busy_for", "location",  "destination", "passenger", "fare"))
 column_types <-        c('numeric', 'character', 'numeric', 'numeric',  'character', 'character',   'character', 'numeric')
 
-unformatted_data <- c(readLines(file))
-d1 <- gsub(", ", "=", unformatted_data)
-d2 <- gsub('"', "", d1)
-d3 <- gsub("\\[", "", d2)
-data <- gsub("\\]", ";", d3)
+format_data <- function(file) {
+  unformatted_data <- c(readLines(file))
+  d1 <- gsub(", ", "=", unformatted_data)
+  d2 <- gsub('"', "", d1)
+  d3 <- gsub("\\[", "", d2)
+  data <- gsub("\\]", ";", d3)
+  data
+}
 
 assign <- function(data, names){
   pairs <- sapply(data, function(i) i)
@@ -22,8 +26,14 @@ assign <- function(data, names){
   result
 }
 
-sx <- lapply(strsplit(data, ";"), strsplit, "=")
-ret <- t(sapply(sx, assign, names))
-colnames(ret) <- names(names)
-res <- as.data.frame(ret)
-res
+data_table <- function(data) {
+  sx <- lapply(strsplit(data, ";"), strsplit, "=")
+  ret <- t(sapply(sx, assign, names))
+  colnames(ret) <- names(names)
+  res <- as.data.frame(ret)
+  res
+}
+
+
+normal_data <- data_table(format_data(normal_file))
+benchmark_data <- data_table(format_data(benchmark_file))
