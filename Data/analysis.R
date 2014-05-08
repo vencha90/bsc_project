49cc3305537b22b40aa7ac6bd79b43e8be48cc22
Karlis Venters <kaarlis.venters@gmail.com>
2014-05-08 12:51:42 +0100

    Improved script readability

--- Data/analysis.R
+++ Data/analysis.R
@@ -4,7 +4,8 @@
 setwd("c:\\Users/user/Dropbox/Documents/ABDN/Y4/PROJECT/Data/")
 file <- "standard/test.log"
 
-names <- setNames(1:8, c("time", "action", "reward", "busy_for", "location", "destination", "passenger", "fare"))
+names <- setNames(1:8, c("time",    "action",    "reward",  "busy_for", "location",  "destination", "passenger", "fare"))
+column_types <-        c('numeric', 'character', 'numeric', 'numeric',  'character', 'character',   'character', 'numeric')
 
 unformatted_data <- c(readLines(file))
 d1 <- gsub(", ", "=", unformatted_data)
@@ -13,13 +14,16 @@ d3 <- gsub("\\[", "", d2)
 data <- gsub("\\]", ";", d3)
 
 assign <- function(data, names){
-  xx <- sapply(data, function(i) i)
-  z <- rep(NA, length(names))
-  z[names[xx[1, ]]] <- xx[2, ]
-  z
+  pairs <- sapply(data, function(i) i)
+  result <- rep(NA, length(names))
+  keys <- names[pairs[1, ]]
+  values <- pairs[2, ]
+  result[keys] <- values
+  result
 }
 
 sx <- lapply(strsplit(data, ";"), strsplit, "=")
 ret <- t(sapply(sx, assign, names))
 colnames(ret) <- names(names)
-ret
+res <- as.data.frame(ret)
+res
